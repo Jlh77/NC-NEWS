e2c1712d17d4461311b45989bda1cd5aaa7d649d
Joe Hawkins <josephlhawkins2@gmail.com>
2022-03-28 12:28:12 +0100

    added route to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const controller = require("./controllers/api.controller");
 app.use(express.json());
 
 app.get("/api/topics", controller.getTopics);
+app.get("/api/articles/:article_id", controller.getTopics);
 
 // Error Handlers
 app.use((req, res) => {
