f0780d53b2f23fa14c453ebecde6f4d82b1c4dc8
Joe Hawkins <josephlhawkins2@gmail.com>
2022-03-28 13:35:41 +0100

    created tests for the route, as well as tests for a 400 and 404 error

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -5,11 +5,6 @@ const request = require("supertest");
 const app = require("../app.js");
 
 afterAll(() => db.end());
-beforeEach(() => seed(testData));
-
-describe("Seed works", () => {
-  test("dummy", () => {});
-});
 
 describe("general 404 error handling", () => {
   test("404 - successful error handling", () => {
@@ -35,6 +30,40 @@ describe("GET /api/topics", () => {
             description: expect.any(String),
           });
         });
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("returns an article object with the below properties", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(article).toMatchObject({
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: 1594329060000,
+          votes: 100,
+        });
+      });
+  });
+  test("returns 404 for non-existent article", () => {
+    return request(app)
+      .get("/api/articles/99999999999999999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
+  test("returns 400 for bad request (non-numeric id passed)", () => {
+    return request(app)
+      .get("/api/articles/not-a-valid-id")
+      .expect(400)
+      .then(({ body }) => {
+        expect(msg).toBe("Bad Request");
       });
   });
 });
