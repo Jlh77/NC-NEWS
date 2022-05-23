444b969bcf9973841b76d65b9fc9ea78f0ab5274
Joe Hawkins <josephlhawkins2@gmail.com>
2022-05-23 15:00:18 +0100

    bad request

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -19,8 +19,10 @@ authRouter.route("/login").post((req, res, next) => {
 authRouter.route("/join").post((req, res, next) => {
   passport.authenticate("local-register", (err, user, info) => {
     if (err) return next(err);
-    if (!user) return res.status(401).send(info);
-    else {
+    if (!user) {
+      console.log("BAD REQUEST? >>>", info);
+      return res.status(401).send(info);
+    } else {
       req.logIn(user, (err) => {
         res.send({ user });
       });
