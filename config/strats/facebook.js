08c5b6fa739498f7c5883085288cbcf9ef6f553e
Joe Hawkins <josephlhawkins2@gmail.com>
2022-05-25 13:33:45 +0100

    framework for fb auth

--- config/strats/facebook.js
+++ config/strats/facebook.js
@@ -5,6 +5,8 @@ const db = require("../../db/connection");
  * FACEBOOK STRATEGY
  *
  * Used for both authenticating and linking of accounts
+ *
+ * IMPORTANT This has not been tested or implemented yet, its just an example of what would be done similar to the google strat.
  */
 
 const FacebookStrategy = require("passport-facebook").Strategy;
@@ -12,9 +14,9 @@ const FacebookStrategy = require("passport-facebook").Strategy;
 passport.use(
   new FacebookStrategy( // Default name facebook
     {
-      clientID: process.env.facebook_APP_ID,
-      clientSecret: process.env.facebook_APP_SECRET,
-      callbackURL: process.env.facebook_CALLBACK_URL,
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
@@ -24,18 +26,19 @@ passport.use(
           req.user.facebook_id = profile.id;
           req.user.facebook_email = profile.email;
 
-          const { rows } = await db.query(
+          await db.query(
             "UPDATE users SET facebook_id = $1, facebook_email = $2 WHERE user_id = $3;",
             [req.user.facebook_id, req.user.facebook_email, req.user.id]
           );
-          // No error (fb account not already in use) link and return updated user
+
+          // No error (facebook account not already in use) link and return updated user
           return done(null, req.user);
         } catch (err) {
           // If facebook account is duplicate (linked to different account) will return error
           return done(null, false, {
             success: false,
             msg: "The Facebook account you tried to link is already associated with another account.",
-          }); //IMPORTANT Error flash not working, fix
+          });
         }
       }
 
@@ -59,50 +62,39 @@ passport.use(
             profile.email,
           ]);
           rows = res.rows;
+          const userIdToLinkTo = rows[0]?.user_id;
 
           if (rows.length) {
-            const existingUser = rows[0];
-            existingUser.facebook_id = profile.id;
-            existingUser.facebook_email = profile.email;
-
-            const rows = await db.query(
-              "UPDATE users SET facebook_id = $1, facebook_email = $2 WHERE user_id = $3;",
-              [
-                existingUser.facebook_id,
-                existingUser.facebook_email,
-                existingUser.user_id,
-              ]
-            );
-            return done(null, existingUser);
-          }
-          // If no existing record, register the user.
-          else {
-            const newUser = {
-              email: profile.email,
-              // facebook account specific fields
-              facebook_id: profile.id,
-              method: "fb", // This field ties this new user to the facebook account
-              // General fields (taken from the stuff facebook gives us)
-              name:
-                profile.name.first_name && profile.name.last_name
-                  ? `${profile.name.first_name} ${profile.name.last_name}`
-                  : "",
-              facebook_email: profile.email,
-              avatar_url: profile.picture,
-            };
-
             const { rows } = await db.query(
-              "INSERT INTO users (email, facebook_id, original_method, name, facebook_email, avatar_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;",
+              "UPDATE users SET facebook_id = $1, facebook_email = $2, facebook_display_name = $3 WHERE user_id = $4 RETURNING *;",
               [
-                newUser.email,
-                newUser.facebook_id,
-                newUser.method,
-                newUser.name,
-                newUser.facebook_email,
-                newUser.avatar_url,
+                profile.id,
+                profile.emails[0].value,
+                profile.displayName,
+                userIdToLinkTo,
               ]
             );
             return done(null, rows[0]);
+          }
+          // If no existing record, register the user.
+          else {
+            const name = profile.name || "";
+
+            // Specific to this platform, will try display_name as username else create random username
+            try {
+              const { rows } = await db.query(
+                "INSERT INTO users (username, email, facebook_id, original_method, name, facebook_email) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;",
+                [
+                  `user${Math.random().toString().substr(2, 8)}`,
+                  profile.email,
+                  profile.id,
+                  "go",
+                  name,
+                  profile.email,
+                ]
+              );
+              return done(null, rows[0]);
+            } catch (err) {}
           }
         } catch (err) {
           return done(err);
